8cec807e3eb7e9a457d0de1920e42ecfa3e631ad
SeamMiner <niatit130@gmail.com>
2020-10-10 13:53:49 +0300

    add map

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HereMap  :center="center" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HereMap from './components/map'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    HereMap
+  },
+  data() {
+    return {
+      // we are this as prop to the HereMap component
+      center:{
+        lat: 40.730610,
+        lng: -73.935242
+      }
+    }
+
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
